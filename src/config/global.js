474f731948a62d42e9d55c8c12b2bf7b7f10c1fc
manuelechavarria <maechavarriaor@gmail.com>
2026-02-19 17:09:37 -0500

    temas

--- src/config/global.js
+++ src/config/global.js
@@ -1,17 +1,19 @@
 export default {
   global: {
-    Name: 'Nombre del recurso educativo',
-    Description: 'Descripción del RED',
-    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.svg'),
-    fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
+    Name: 'Comprensión lectora mediante análisis crítico',
+    Description:
+      'El componente formativo integra técnicas para comprender textos mediante análisis crítico, así como estrategias de lectura activa. Aborda la identificación de ideas principales, evaluación de argumentos y construcción de opiniones fundamentadas; asimismo, desarrolla habilidades reflexivas. De esta manera, se fortalecen competencias para interpretar textos en diversos contextos.',
+
+    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.png'),
+    fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.jpg'),
     imagenesDecorativasBanner: [
       {
         clases: ['banner-principal-decorativo-1', 'd-none', 'd-lg-block'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-1.svg'),
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-1.png'),
       },
       {
         clases: ['banner-principal-decorativo-2'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.svg'),
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.png'),
       },
     ],
   },
@@ -31,46 +33,93 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Tema 1',
+        titulo: 'Técnicas de comprensión lectora',
         desarrolloContenidos: true,
         subMenu: [
           {
             numero: '1.1',
-            titulo: 'Subtema 1',
+            titulo: 'Estrategias de lectura',
             hash: 't_1_1',
+          },
+          {
+            numero: '1.2',
+            titulo: 'Señales de organización textual',
+            hash: 't_1_2',
+          },
+          {
+            numero: '1.3',
+            titulo: 'Técnica del subrayado',
+            hash: 't_1_3',
           },
         ],
       },
-
       {
         nombreRuta: 'tema2',
         numero: '2',
-        titulo: 'Tema 2',
+        titulo: 'Comprensión global de textos',
         desarrolloContenidos: true,
+        subMenu: [],
       },
       {
         nombreRuta: 'tema3',
         numero: '3',
-        titulo: 'Tema 3',
+        titulo: 'Comprensión específica de textos ',
         desarrolloContenidos: true,
+        subMenu: [],
       },
       {
         nombreRuta: 'tema4',
         numero: '4',
-        titulo: 'Tema 4',
+        titulo: 'Análisis de oraciones',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '4.1',
+            titulo:
+              'Estrategias para identificar la intencionalidad comunicativa',
+            hash: 't_4_1',
+          },
+          {
+            numero: '4.2',
+            titulo: 'Estrategias para identificar relaciones entre partes',
+            hash: 't_4_2',
+          },
+          {
+            numero: '4.3',
+            titulo: 'Estrategias para identificar la idea subyacente ',
+            hash: 't_4_3',
+          },
+        ],
       },
       {
         nombreRuta: 'tema5',
         numero: '5',
-        titulo: 'Tema 5',
+        titulo: 'Competencias de lectura crítica',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '5.1',
+            titulo: 'Contenidos locales del texto',
+            hash: 't_5_1',
+          },
+          {
+            numero: '5.2',
+            titulo: 'Sentido global del texto',
+            hash: 't_5_2',
+          },
+          {
+            numero: '5.3',
+            titulo: 'Reflexión y evaluación textual',
+            hash: 't_5_3',
+          },
+        ],
       },
       {
         nombreRuta: 'tema6',
         numero: '6',
-        titulo: 'Tema 6',
+        titulo: 'Aplicación de técnicas de lectura',
         desarrolloContenidos: true,
+        subMenu: [],
       },
     ],
     subMenu: [
@@ -91,11 +140,7 @@ export default {
         icono: 'fas fa-sort-alpha-down',
         titulo: 'Glosario',
       },
-      {
-        nombreRuta: 'complementario',
-        icono: 'far fa-folder-open',
-        titulo: 'Material complementario',
-      },
+
       {
         icono: 'fas fa-book',
         titulo: 'Referencias bibliográficas',
@@ -128,13 +173,111 @@ export default {
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Definición',
+      termino: 'Actividad cognitiva',
+      significado:
+        'proceso mental para analizar, relacionar y reflexionar sobre información.',
+    },
+    {
+      termino: 'Conector',
+      significado: 'palabra que une ideas y muestra su relación.',
+    },
+    {
+      termino: 'Enfoque crítico',
+      significado:
+        'capacidad de evaluar y emitir juicios sobre un texto leído.',
+    },
+    {
+      termino: 'Función persuasiva',
+      significado:
+        'uso del lenguaje para influir en ideas o acciones del lector.',
+    },
+    {
+      termino: 'Idea subyacente',
+      significado: 'mensaje implícito que el autor comunica indirectamente.',
+    },
+    {
+      termino: 'Inferencia',
+      significado: 'conclusión obtenida a partir de información del texto.',
+    },
+    {
+      termino: 'Lectura crítica',
+      significado: 'lectura reflexiva para analizar y valorar un texto.',
+    },
+    {
+      termino: 'Metacognitivo',
+      significado:
+        'proceso para evaluar y controlar la propia forma de aprender.',
+    },
+    {
+      termino: 'Propósito comunicativo',
+      significado: 'intención del autor al producir un texto.',
+    },
+    {
+      termino: 'Sentido global',
+      significado:
+        'visión total del texto a partir de la relación de sus ideas.',
+    },
+    {
+      termino: 'Sentido global',
+      significado:
+        'visión total del texto a partir de la relación de sus ideas.',
     },
   ],
   referencias: [
     {
-      referencia: '',
+      referencia:
+        'Carriazo, M. M., & Martínez, L. (2010). <em>Curso de lectura crítica</em>: <em>Estrategias de comprensión lectora</em>. Autor. Ecuador.',
+      link: '',
+    },
+    {
+      referencia:
+        'Cassany, D. (2003). Aproximaciones a la lectura crítica: teoría, ejemplos y reflexiones. <em>Tarbiya:</em> <em>Revista de Investigación e Innovación Educativa</em>, (32), pp. 1-20.',
+      link: 'https://revistas.uam.es/tarbiya/article/viewFile/7275/7623',
+    },
+    {
+      referencia:
+        'De Zubiría, M. (1995). <em>Teoría de las seis lecturas</em>: Mecanismos del aprendizaje semántico. <em>Tomo 1: Preescolar y primaria</em>. FAMDI. Bogotá, Colombia.',
+      link: '',
+    },
+    {
+      referencia:
+        'De Zubiría, M. (2002). <em>Enfoques pedagógicos y didácticas contemporáneas</em>. Manuscrito no publicado.',
+      link:
+        'https://www.academia.edu/13959300/Enfoques_Pedag%C3%B3gicos_y_Did%C3%A1cticas_Contempor%C3%A1neas',
+    },
+    {
+      referencia:
+        'García, C., Gutiérrez, M. C., y Condemarín, E. (2000). <em>A estudiar se aprende</em>: <em>Metodología de estudio. </em>(4.ª ed.). Alfaomega. Chile.',
+      link: '',
+    },
+    {
+      referencia:
+        'Hernández, M. T., y Vélez, A. (Eds.). (2012). <em>Método lúdico de comprensión lectora</em> (Vol. 2). Fundación Lee. Colombia.',
+      link: '',
+    },
+    {
+      referencia:
+        'Instituto Colombiano para la Evaluación de la Educación. (2016). <em>Módulo de lectura crítica: Guía de orientación Saber Pro 2016-2.</em>',
+      link: '',
+    },
+    {
+      referencia:
+        'Serrano de Moreno, S., & Madrid de Forero, A. (2007). Competencias de lectura crítica: Una propuesta para la reflexión y la práctica. <em>Acción Pedagógica</em>, (16), pp. 58-68.',
+      link: 'https://dialnet.unirioja.es/descarga/articulo/2968602.pdf',
+    },
+    {
+      referencia:
+        'Se ajusta el contenido del documento según Planeación pedagógica y normas APA 7 edición.',
+      link: '',
+    },
+    {
+      referencia:
+        'Serrano de Moreno, S., & Madrid de Forero, A. (2007). Competencias de lectura crítica: Una propuesta para la reflexión y la práctica. <em>Acción Pedagógica</em>, (16), pp. 58-68.',
+      link: 'https://dialnet.unirioja.es/descarga/articulo/2968602.pdf',
+    },
+    {
+      referencia:
+        'Se ajusta el contenido del documento según Planeación pedagógica y normas APA 7 edición.',
       link: '',
     },
   ],
